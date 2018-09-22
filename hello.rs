bb0b42619a5ae841e78183f80d81f17fc4306d3b
bearachute <sjourand@gmail.com>
2018-09-21 21:10:20 -0400

    huh?

---
+++ hello.rs
@@ -0,0 +1,5 @@
+// A comment
+fn main() {
+    println!("Hi world!");
+    println!("I'm a goofy goober yah!")
+}
---
+++ hello2.rs
@@ -0,0 +1,11 @@
+fn main() {
+// these comments effect nothing
+// uncomment to have this work
+// println!("Hi world");
+/* I guess there's no style to these
+* so these even work
+* println!("no");
+*/
+let x = 5 + /* 90 + */ 5;
+println!("Is `x` or 100? x = {}", x);
+}
